cd72b55e1a288d5d576da916f235a66d24198d80
Gracieful <g.hartywork@gmail.com>
2022-11-10 17:31:50 +0000

    First

--- vscode-template-main/raylibTemplate.cpp
+++ vscode-template-main/raylibTemplate.cpp
@@ -2,13 +2,33 @@
 
 int main()
 {
+    const int windowWidth = {400};
+    const int windowHeight = {400};
+
 
     InitWindow(250,250, "Grace is Testing");
+
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RED);
         SetTargetFPS(60);
+
+
+        Texture2D scarfy = LoadTexture("resources/scarfy.png");
+        Rectangle scarfyRec;
+        scarfyRec.width = scarfy.width/6;
+        scarfyRec.height = scarfy.height;
+        scarfyRec.x = 0;
+        scarfyRec.y = 0; 
+        Vector2 scarfyPos;
+        scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
+        scarfyPos.y = windowHeight - scarfyRec.height;
+
+        DrawTextureRec(scarfy, scarfyRec,scarfyPos, WHITE);
+    
+    UnloadTexture(scarfy);
+    EndDrawing();
     }
 
 CloseWindow();
